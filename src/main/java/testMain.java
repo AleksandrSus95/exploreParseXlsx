e0d3192e08262e2523a2ccd1ebd6e9989df092f3
aleksandr <a.suslov@elma-bpm.com>
2022-03-05 09:41:42 +0400

    exp Iterators

--- src/main/java/testMain.java
+++ src/main/java/testMain.java
@@ -1,27 +1,26 @@
 import org.apache.poi.ss.usermodel.*;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import javax.annotation.processing.SupportedSourceVersion;
 import java.io.*;
 import java.nio.file.Paths;
 import java.util.Iterator;
 
-public class testmain {
+public class testMain {
     public static void main(String[] args) {
         File file = new File(Paths.get(System.getProperty("user.dir")).toString(), "test.xlsx");
+        String elementsExport = "";
         try {
             FileInputStream fileInputStream = new FileInputStream(file);
             Workbook workbook = new XSSFWorkbook(fileInputStream);
             Sheet datatypeSheet = workbook.getSheetAt(0);
-            Iterator iterator = datatypeSheet.iterator();
-
+            Iterator<Row> iterator = datatypeSheet.iterator();
+            iterator.next();
             while (iterator.hasNext()) {
-                Row currentRow = (Row) iterator.next();
-                Iterator cellIterator = currentRow.iterator();
-                cellIterator.hasNext();
+                Row currentRow = iterator.next();
+                Iterator<Cell> cellIterator = currentRow.iterator();
                 while (cellIterator.hasNext()) {
-                    Cell currentCell = (Cell) cellIterator.next();
-                    System.out.print(currentCell);
+                    Cell currentCell = cellIterator.next();
+                    elementsExport += currentCell.toString();
                 }
             }
         } catch (FileNotFoundException e) {
@@ -29,6 +28,7 @@ public class testmain {
         } catch (IOException e) {
             e.printStackTrace();
         }
+        System.out.println(elementsExport);
     }
 }
 
